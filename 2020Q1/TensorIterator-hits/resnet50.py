9dd38a4d6325e9d914827180135134c885687d35
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-01-27 12:50:43 -0800

    save

--- 2020Q1/TensorIterator-hits/resnet50.py
+++ 2020Q1/TensorIterator-hits/resnet50.py
@@ -1,16 +1,12 @@
 import torchvision
 import torch
 
-print("running resnet50")
 model = torchvision.models.resnet50().cuda()
 input_ = torch.randn(64, 3, 224, 224, device="cuda")
 torch.cuda.synchronize()
 
-print("==============================")
-print("forward")
 output_ = model(input_)
 torch.cuda.synchronize()
-print("end forward")
 print("==============================")
 
 target = torch.empty(64, dtype=torch.long, device="cuda").random_(1000)
@@ -19,15 +15,10 @@ o = torch.optim.Adam(model.parameters())
 torch.cuda.synchronize()
 
 print("==============================")
-print("backward")
 l = loss(output_, target)
 l.backward()
 torch.cuda.synchronize()
-print("end backward")
 print("==============================")
 
-print("optimizer.step()")
 o.step()
 torch.cuda.synchronize()
-print("end optimizer.step()")
-print("==============================")
