110734197a656114b135df478936c985e264e1b1
Xiang Gao <qasdfgtyuiop@gmail.com>
2022-01-27 14:25:05 -0800

    save

---
+++ 2022/benchmark-topk/benchmark-sort.py
@@ -0,0 +1,33 @@
+import torch
+import time
+import torch
+import pandas as pd
+import numpy as np
+import torch.utils.benchmark as benchmark
+
+
+torch.manual_seed(1)
+dtype = torch.float
+data = []
+
+for d1 in range(1, 17):
+    D2 = [100000, 200000, 300000, 400000, 500000]
+    for d2 in D2:
+        k = 2000
+        print(f"----------------- D1 = {d1}, D2 = {d2} -----------------")
+        try:
+            x = torch.randn((d1, d2), dtype=dtype, device="cuda")
+            m = benchmark.Timer(
+                stmt='x.topk(k=k, dim=1, sorted=False, largest=True)',
+                globals={'x': x, 'k': k},
+                num_threads=1,
+            ).blocked_autorange(min_run_time=1)
+            print(m)
+            time_ms = m.median * 1000
+        except RuntimeError: # OOM
+            time_ms = -1
+        data.append([d1, d2, k, time_ms])
+
+df = pd.DataFrame(data=data, columns=['D1', 'D2', 'k', 'time(ms)'])
+print(df)
+df.to_csv('benchmark.csv')
